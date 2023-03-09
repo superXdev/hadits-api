baebf3027a20af687e5a322ab5a4e4079ad1dbb5
Fikri <fikrizhanking@gmail.com>
2023-03-09 19:24:01 +0700

    Add completed database & rest API

---
+++ index.js
@@ -0,0 +1,152 @@
+const express = require('express');
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const Op = Sequelize.Op;
+
+// Initialize express app
+const app = express();
+
+// Connect to SQLite database
+const sequelize = new Sequelize({
+   dialect: 'sqlite',
+   storage: 'data.db',
+   logging: false
+});
+
+// Define model for Hadis table
+class Hadis extends Model {}
+Hadis.init({
+   id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+   },
+   nomor: DataTypes.INTEGER,
+   perawi: DataTypes.STRING,
+   arabic: DataTypes.TEXT,
+   terjemahan: DataTypes.TEXT,
+   riwayat: DataTypes.STRING
+}, {
+   sequelize,
+   modelName: 'hadis',
+   timestamps: false
+});
+
+// Define pagination function
+const paginate = async (query, { page, limit }) => {
+   const offset = page * limit;
+   const count = await Hadis.count();
+   const pageCount = Math.ceil(count / limit);
+   return {
+      items: query.offset(offset).limit(limit),
+      meta: {
+         count,
+         pageCount,
+         page,
+         pageSize: limit
+      }
+   };
+};
+
+// Define routes
+app.get('/', async (req, res) => {
+   try {
+      const page = parseInt(req.query.page) || 1;
+      const limit = parseInt(req.query.limit) || 10;
+
+      const result = await Hadis.findAndCountAll({
+         offset: (page - 1) * limit,
+         limit
+      });
+
+      const pageCount = Math.ceil(result.count / limit);
+      const response = {
+         items: result.rows,
+         meta: {
+            total: result.count,
+            totalPage: pageCount,
+            page,
+            pageSize: limit
+         }
+      };
+
+      res.json(response);
+   } catch (error) {
+      console.log(error.message)
+      res.status(500).json({ error: 'Something went wrong' });
+   }
+});
+
+app.get('/:perawi/:nomor', async (req, res, next) => {
+   try {
+      const perawi = req.params.perawi;
+      const nomor = parseInt(req.params.nomor);
+
+      const result = await Hadis.findOne({
+         where: { perawi, nomor }
+      });
+
+      if (!result) {
+         return res.status(404).json({ message: 'Hadits not found' });
+      }
+
+      res.json(result);
+   } catch (error) {
+      console.log(error.message)
+      res.status(500).json({ error: 'Something went wrong' });
+   }
+});
+
+app.get('/id/:id', async (req, res) => {
+   const id = req.params.id;
+   const hadis = await Hadis.findByPk(id);
+   if (hadis) {
+      res.json(hadis);
+   } else {
+      res.status(404).json({ error: 'Hadits not found' });
+   }
+});
+
+app.get('/search', async (req, res, next) => {
+   try {
+      const q = req.query.q || '';
+      const page = parseInt(req.query.page) || 1;
+      const limit = parseInt(req.query.limit) || 10;
+
+      // Generate the where condition
+      const where = {};
+      if (q) {
+         where.terjemahan = { [Op.like]: `%${q}%` };
+      }
+
+      const result = await Hadis.findAndCountAll({
+         where,
+         offset: (page - 1) * limit,
+         limit
+      });
+
+      const pageCount = Math.ceil(result.count / limit);
+      const response = {
+         items: result.rows,
+         meta: {
+         total: result.count,
+         totalPage: pageCount,
+            page,
+            pageSize: limit,
+            query: q
+         }
+      };
+
+      res.json(response);
+   } catch (error) {
+      console.log(error.message)
+      res.status(500).json({ error: 'Something went wrong' });
+   }
+});
+
+
+// Start the server
+sequelize.sync().then(() => {
+   app.listen(3000, () => {
+      console.log('Server started on http://localhost:3000');
+   });
+});
--- scrap.js
+++ scrap.js
@@ -1,6 +1,7 @@
 const cheerio = require('cheerio');
 const Sequelize = require('sequelize');
 const request = require('request-promise')
+const input = require('input');
 require('dotenv').config();
 
 const sequelize = new Sequelize({
@@ -19,6 +20,17 @@ const Hadis = sequelize.define('Hadis', {
   timestamps: false
 });
 
+/* 
+   - bukhari: 7008 *
+   - muslim: 5362 *
+   - abudaud: 4590 *
+   - tirmidzi: 3891 *
+   - nasai: 5662 *
+   - ibnumajah: 4332 *
+   - malik: 1594 *
+   - ahmad: 15070 *
+   - darimi: 3367 *
+*/
 
 (async function() {
    // sync database
@@ -26,59 +38,49 @@ const Hadis = sequelize.define('Hadis', {
 
    // await Hadis.destroy({ truncate: true });
 
-   // const list = ['bukhari', 'muslim', 'abudaud', 'tirmidzi', 'nasai', 'ibnumajah', 'malik', 'ahmad', 'darimi'];
-   const list = ['bukhari', 'muslim'];
+   const p = await input.text('Hadits:');
+   let page = parseInt(await input.text('From page:'));
 
-   for(p of list) {
-      console.log('Scrapping hadis:', p);
-      let page = 1857;
-      let hadis = []
+   console.log('Scrapping hadis:', p);
 
-      // scrapping over page
-      while(true) {
-         try {
-            const url = `https://hadits.in/${p}/${page}`;
+   // scrapping over page
+   while(true) {
+      try {
+         const url = `https://hadits.in/${p}/${page}`;
 
-            const data = await request({
-               url: url,
-               proxy: process.env.ROTATING_PROXY
-            });
-         
-            if(data) {
-               console.log('[', p, '] Halaman:', page);
-               const $ = cheerio.load(data);
-
-               // stop scrapping
-               if($('title').text() === 'Ensiklopedi Hadits - Kitab 9 Imam') {
-                  console.log('Total:', page - 1);
-                  page = 1;
-                  break;
-               }
-
-               // get info
-               const arabic = $('#arabic_container').text();
-               const arti = $('#terjemah_container').text();
-               const hr = $('[property="og:title"]').attr('content');
-
-               Hadis.create({
-                  nomor: page,
-                  perawi: p,
-                  arabic: arabic,
-                  terjemahan: arti,
-                  riwayat: hr
-               })
-
-               page++;
-            }
-         } catch(err) {
-            console.log(err.message);
-         }
-         
-      }
-
-      // insert data
-      await Hadis.bulkCreate(hadis)
+         const data = await request({
+            url: url,
+            proxy: process.env.ROTATING_PROXY
+         });
       
+         if(data) {
+            console.log('[', p, '] Halaman:', page);
+            const $ = cheerio.load(data);
+
+            // stop scrapping
+            if($('title').text() === 'Ensiklopedi Hadits - Kitab 9 Imam') {
+               console.log('Total:', page - 1);
+               break;
+            }
+
+            // get info
+            const arabic = $('#arabic_container').text();
+            const arti = $('#terjemah_container').text();
+            const hr = $('[property="og:title"]').attr('content');
+
+            Hadis.create({
+               nomor: page,
+               perawi: p,
+               arabic: arabic,
+               terjemahan: arti,
+               riwayat: hr
+            });
+
+            page++;
+         }
+      } catch(err) {
+         console.log(err.message);
+      }
    }
    
 })();
